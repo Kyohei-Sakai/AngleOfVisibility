58aa7cadf2c46d98871b566a49de5c4b5556cd4a
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2016-12-01 18:40:59 +0900

    Add Cube and Camera

---
+++ Assets/AnfleOfVisibility.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnfleOfVisibility : MonoBehaviour {
+
+	public Camera eye;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (eye.aspect);
+		eye.aspect = 1.0f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Transform.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transform : MonoBehaviour {
+
+	public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (cube.name);
+//		Debug.Log (cube.transform.GetType);
+		Debug.Log (cube.transform.position.x);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
