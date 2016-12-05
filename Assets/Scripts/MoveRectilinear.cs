feaa175d2f3faee98f068abd3fb61af7f019cfc7
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2016-12-05 14:06:28 +0900

    Add new Scene and Script to move object

---
+++ Assets/Scripts/MoveRectilinear.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRectilinear : MonoBehaviour {
+
+	public GameObject obj;
+
+	public float velocity = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		if (obj == null) {
+			return;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = obj.transform.position;
+		pos.z += velocity;
+		obj.transform.position = pos;
+
+	}
+}
